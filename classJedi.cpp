0f2aec53e0595796d02b5dab012574d27eb51fa1
Georgi1212 <72854235+Georgi1212@users.noreply.github.com>
2021-03-25 23:08:22 +0200

    Update classJedi.cpp

--- classJedi.cpp
+++ classJedi.cpp
@@ -1,14 +1,15 @@
+#include "classPlanet.hpp"
 #include "classJedi.hpp"
 
-void Jedi::printJedi() const
+/*void Jedi::printJedi() const
 {
 	cout << "Name of the Jedi: " << name << endl;
 	cout << "JediRank: " << rank << endl;
 	cout << "Midi-chlorian: " << midi_chlorian << endl;
- //	planet.printPlanet(const Pl& planet);
+ 	printPlanet(planet);
 	cout << "Spicies: " << spicies << endl;
 	cout << "Military rank: " << militaryRank << endl;						
-}
+}*/
 
 void Jedi::setJediName(const char* other_jediName)
 {
@@ -129,17 +130,27 @@ Jedi& Jedi::operator=(const Jedi& other)
 		
 		this->midi_chlorian = other.midi_chlorian;
 		
-		
 		delete[] spicies;
 		this->spicies = new char[strlen(other.spicies) + 1];
 		strcpy(this->spicies, other.spicies);
-		
 		
 		delete[] militaryRank;
 		this->militaryRank = new char[strlen(other.militaryRank) + 1];
 		strcpy (this->militaryRank, other.militaryRank);
 	}
 	return *this;
+}
+
+bool Jedi:: operator==(const Jedi& other) const
+{ 
+	return !strcmp(this->name, other.name) && 
+			!strcmp(this->spicies, other.spicies) && 
+			!strcmp(this->militaryRank, other.militaryRank);
+}
+
+bool Jedi:: operator!=(const Jedi& other) const
+{
+	return !(*this == other);
 }
 
 Jedi::~Jedi()
@@ -153,3 +164,68 @@ Jedi::~Jedi()
 	militaryRank = NULL;
 }
 
+ostream& operator<<(ostream& os, const Jedi& jedi)
+{
+	os << jedi.name << endl;
+	os << jedi.midi_chlorian << endl;
+	os << jedi.planet.getPlanetName() << endl;
+	os << jedi.spicies << endl;
+	os << jedi.militaryRank << endl;
+	
+	switch (jedi.rank)
+	{
+		case(JediRank)0: os << "Youngling" << endl; break;
+		case(JediRank)1: os << "Padawan" << endl; break;
+		case(JediRank)2: os << "JediKnight" << endl; break;
+		case(JediRank)3: os << "JediGuardian" << endl; break;
+		case(JediRank)4: os << "JediMaster" << endl; break;
+		case(JediRank)5: os << "GrandMaster" << endl; break;
+	
+		default: os << "There's no such planet" << endl; break;
+	}
+  
+  return os;
+}
+
+istream& operator>>(istream& is, Jedi& jedi)
+{
+	delete[] jedi.name;
+	delete[] jedi.spicies;
+	delete[] jedi.militaryRank;
+	
+	char* buffer = new char[100];
+	int temp;
+	
+	//cout << "Jedi name:";
+	is.getline(buffer, 100);
+	jedi.name = new char[strlen(buffer) + 1];
+	strcpy(jedi.name, buffer);
+	
+	//cout << "Jedi rank:";
+	is >> temp;
+	jedi.rank = (JediRank)temp;
+	
+	//cout << "Jedi midi_chlorian:";
+	is >> jedi.midi_chlorian;
+	
+	is.ignore();
+	
+	//Planet;
+	is >> jedi.planet;
+	
+	//cout << "Jedi spicies:";
+	is.getline(buffer, 100);
+	jedi.spicies = new char[strlen(buffer) + 1];
+	strcpy(jedi.spicies, buffer);
+	
+	//cout << "Jedi military rank:";
+	is.getline(buffer, 100);
+	jedi.militaryRank = new char[strlen(buffer) + 1];
+	strcpy(jedi.militaryRank, buffer);
+	
+	is.ignore();
+	
+	delete[] buffer;
+	
+	return is;
+}
