b680d37692808c3c02fd227dcdf7f39b2ff34eb6
Georgi1212 <72854235+Georgi1212@users.noreply.github.com>
2021-03-25 23:11:20 +0200

    Update classPlanet.hpp

--- classPlanet.hpp
+++ classPlanet.hpp
@@ -1,45 +1,51 @@
+#pragma once
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
 
-class Planet{
-
-/*enum KindOfPlanet { 
+enum KindOfPlanet { 
 	ChthonianPlanet, CarbonPlanet, CorelessPlanet, DesertPlanet, GasDwarfPlanet, GasGiantPlanet, HeliumPlanet, IceGiantPlanet, IcePlanet,
 	IronPlanet, LavaPlanet, OceanPlanet, ProtoPlanet, PuffyPlanet, SilicatePlanet, TerrestrialPlanet};
-};*/
-
+	
+class Planet{
+	
 private:
 	
 	char* name;
 	char* planetSystem;
 	char* republic;
-	
-	enum KindOfPlanet { 
-	ChthonianPlanet, CarbonPlanet, CorelessPlanet, DesertPlanet, GasDwarfPlanet, GasGiantPlanet, HeliumPlanet, IceGiantPlanet, IcePlanet,
-	IronPlanet, LavaPlanet, OceanPlanet, ProtoPlanet, PuffyPlanet, SilicatePlanet, TerrestrialPlanet};
+	KindOfPlanet kindPlanet;
 	
 public:
 	
 	void setPlanetName(const char* other_name);
 	void setPlanetSystem(const char* other_planetSystem);
 	void setRepublic(const char* other_republic);
+	void setKindOfPlanet(const KindOfPlanet _kindPlanet);
 	
 	char* getPlanetName() const;
 	char* getPlanetSystem() const;
 	char* getRepublic() const;
+	KindOfPlanet getKindOfPlanet() const;
 		
 	Planet();
-	Planet(char* _name, char* _planetSystem, char* _republic);
+	Planet(const char* _name, const char* _planetSystem, const char* _republic, const KindOfPlanet _kindPlanet);
 	Planet(const Planet& other);
 	Planet& operator=(const Planet& other);
+	bool operator==(const Planet& other) const;
+	bool operator!=(const Planet& other) const;
 	~Planet();
 	
 	friend ostream& operator<<(ostream& os, const Planet& planet);
-	
-	template<typename Pl>
-	void printPlanet(const Pl& planet) const;
+	friend istream& operator>>(istream& is, Planet& planet);
+
 	
 };
+
+template<typename PrintPlanet>
+void printPlanet(PrintPlanet& planet)
+{
+	cout << planet << endl;
+}
